82d278e86f41af76d10cd3ad446359e26498b397
idler <57109313@qq.com>
2018-07-17 15:58:21 +0800

    mirror handle

--- mirror.sh
+++ mirror.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-master_host='192.168.40.80 registry.group.idler.com'
-
 function install () {
-if cat /etc/hosts | grep "${master_host}">/dev/null
-    then
-      echo "hosts master_host exist"
-    else
-      echo "${master_host}" >> /etc/hosts
-  fi
-  curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s https://registry.group.idler.com
-
+ sed -i '/registry.group.idler.com/d' /etc/hosts
+ echo "${master_host}" >> /etc/hosts
+ curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s https://registry.group.idler.com
 }
 
-install
+function isIp () {
+  if [[ "$1" =~ ^([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$ ]];then
+    return 1
+  else
+    return 0
+  fi
+}
 
-#sed -r "${master_host}/d"  filename
+if [ $# == 1 ] ; then
+  isIp "$1"
+  if [ "$?" == "1" ] ; then
+    master_host="$1 registry.group.idler.com"
+    install
+  else
+    echo "$1 not a ip"
+    exit 0
+  fi
+else
+  echo "usage \"mirror.sh ip\", eg. mirror.sh 192.168.40.80"
+fi
