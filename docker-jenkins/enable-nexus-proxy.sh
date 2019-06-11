eca7b6386b8233e6cc3338ab71ae22c093ef53f3
Administrator <57109313@qq.com>
2019-06-11 08:40:23 +0800

    jenkins调整

--- docker-jenkins/enable-nexus-proxy.sh
+++ docker-jenkins/enable-nexus-proxy.sh
@@ -3,7 +3,11 @@
 function uninstall () {
   echo "remove nexus host to registry service."
   docker service update -d \
-      --host-rm nexus.idler.com
+      --host-rm nexus.idler.com \
+      --host-rm registry.idler.com \
+      --host-rm registry.group.idler.com \
+      --secret-rm lb_domain.group.crt \
+      --secret-rm lb_domain.group.key \
       jenkins_jenkins
 }
 
@@ -11,6 +15,10 @@ function install () {
   echo "update nexus host to registry service."
   docker service update -d \
     --host-add nexus.idler.com:192.168.225.5 \
+    --host-add registry.idler.com:192.168.225.5 \
+    --host-add registry.group.idler.com:192.168.225.5 \
+    --secret-add source=lb_domain.crt,target=/etc/docker/certs.d/registry.idler.com/ca.crt \
+    --secret-add source=lb_domain.group.crt,target=/etc/docker/certs.d/registry.group.idler.com/ca.crt \
     jenkins_jenkins
 }
 
